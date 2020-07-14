569e4b31a22b1ab3421078420ba83d08ccab8394
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-14 08:28:03 -0300

    separando por palavras

--- 01_projeto_palavrasMaisUsadas/index.js
+++ 01_projeto_palavrasMaisUsadas/index.js
@@ -18,17 +18,25 @@ const simbolos = [
   "(",
   ")",
 ];
+
+const mesclarElementos = (array) => array.join(" ");
+
+const separarPorLinha = (todoConteudo) => todoConteudo.split("\n");
+const separarPorPalavras = (todoConteudo) => todoConteudo.split(" ");
 fn.lerDiretorio(caminho)
   //.then((arquivos) => fn.elementosTermincadoCom(".srt")(arquivos))
   .then(fn.elementosTermincadoCom(".srt"))
   // .then((arquviosSTR) => fn.lerArquivos(arquviosSTR))
   .then(fn.lerArquivos)
-  .then((conteudos) => conteudos.join("\n"))
-  .then((todoConteudo) => todoConteudo.split("\n"))
+  .then(mesclarElementos)
+  .then(separarPorLinha)
   // .then((linhas) => fn.removerSeVazio(linhas))
   .then(fn.removerElementosSeVazio)
   //.then((linhas) => fn.removerElementosSeIncluir("-->")(linhas)) //assim ou
   .then(fn.removerElementosSeIncluir("-->")) //assim
   .then(fn.removerElementosSeNumero)
   .then(fn.removerSimbolos(simbolos))
+  .then(mesclarElementos)
+  .then(separarPorPalavras)
+  .then(fn.removerElementosSeVazio)
   .then(console.log);
