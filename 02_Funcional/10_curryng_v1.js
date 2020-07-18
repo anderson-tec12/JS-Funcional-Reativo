bc0b6cdc948562d5402ccd03007e1a78d9fa18e5
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-18 20:47:18 -0300

    teste

---
+++ 02_Funcional/10_curryng_v1.js
@@ -0,0 +1,21 @@
+function somar(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+console.log(somar(1)(2)(3));
+
+function textoComTamanhoEntre(min, max, erro, texto) {
+  const tamanho = (texto || "").trim().length;
+
+  if (tamanho < min || tamanho > max) {
+    throw erro;
+  }
+}
+
+const p1 = { nome: "A", preco: 14.99, desc: 0.25 };
+
+textoComTamanhoEntre(4, 255, "Nome invalido", p1.nome);
