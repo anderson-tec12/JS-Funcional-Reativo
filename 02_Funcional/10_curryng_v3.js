39a6151b6745a4d6445d01a62479a7f1ac89bdca
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-19 20:36:41 -0300

    Curryng

--- 02_Funcional/10_curryng_v3.js
+++ 02_Funcional/10_curryng_v3.js
@@ -6,7 +6,7 @@ function somar(a) {
   };
 }
 
-console.log(somar(1)(2)(3));
+//console.log(somar(1)(2)(3));
 
 function textoComTamanhoEntre(min) {
   return function (max) {
@@ -16,7 +16,7 @@ function textoComTamanhoEntre(min) {
         const tamanho = (texto || "").trim().length;
 
         if (tamanho < min || tamanho > max) {
-          throw erro;
+          throw true;
         }
       };
     };
@@ -28,5 +28,23 @@ const p1 = { nome: "A", preco: 14.99, desc: 0.25 };
 textoComTamanhoEntre(4)(255)("Nome invalido__")(p1.nome);
 
 const forcarTamanhoPadrao = textoComTamanhoEntre(4)(255);
+const forcarNomeProdutoValido = forcarTamanhoPadrao("Nome produto invalido");
 
 forcarTamanhoPadrao("Nome invalido__")(p1.nome);
+
+function aplicarValidacao(fn) {
+  //lazy evaluation
+  return function (valor) {
+    try {
+      fn(valor);
+    } catch (e) {
+      return { error: e };
+    }
+  };
+}
+
+const validarNomeProduto = aplicarValidacao(forcarNomeProdutoValido);
+
+console.log("#####################################################");
+
+console.log(validarNomeProduto(p1.nome));
