1847a80a736ed7ba4580e9a6fdb136cbb3a360f0
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-24 08:33:12 -0300

    desafio

---
+++ 04_rxjs/06_desafio.js
@@ -0,0 +1,18 @@
+//DEsafio
+//Criar uma strem de numeros
+// entre min e max com Observable
+const { Observable, noop } = require("rxjs");
+
+function entre(min, max) {
+  return new Observable((subscriber) => {
+    //gerando o array
+    Array(max - min)
+      .fill()
+      .map((_, i) => subscriber.next(min + i));
+    // for (let i = min; i <= max; i++) {
+    //   subscriber.next(i);
+    // }
+    subscriber.complete();
+  });
+}
+entre(4, 10).subscribe((num) => console.log(`num = ${num}`), noop, console.log);
